7e2f61a3955ae47a6aad671dc93e2f2d6c63d48e
Moin Bagban <moinbagban81@gmail.com>
2026-04-11 21:58:27 +0530

    added prisma plugin

---
+++ src/plugins/prisma.js
@@ -0,0 +1,24 @@
+import fp from "fastify-plugin";
+import { PrismaClient } from "../../generated/prisma/client.ts";
+
+const prisma = new PrismaClient({
+    log: ["error", "warn"],
+});
+
+async function prismaPlugin (fastify) {
+
+    try {
+        await prisma.$connect();
+    } catch (err) {
+        fastify.log.error("Failed to connect to the database:", err);
+        throw err;
+    }
+    
+    fastify.decorate("prisma", prisma);
+
+    fastify.addHook("onClose", async (instance) => {
+        await instance.prisma.$disconnect();
+    });
+}
+
+export default fp(prismaPlugin);
