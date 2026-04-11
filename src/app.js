9028ae62a3058dd5aa711e95f5b921ee6d33866f
Moin Bagban <moinbagban81@gmail.com>
2026-04-11 21:59:13 +0530

    added plugins and routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,33 @@
 import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
+import fastifyCookie from "@fastify/cookie";
+import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
+import prismaPlugin from "./plugins/prisma.js";
+import userRoutes from "./modules/users/users.routes.js";
+import { JWT_SECRET, JWT_ALGORITHM } from "./config/env.js";
 
 export async function buildApp() {
     const app = fastify({
         logger: true,
     });
+
+    app.setValidatorCompiler(validatorCompiler);
+    app.setSerializerCompiler(serializerCompiler);
+
+    await app.register(fastifyJwt, {
+        secret: JWT_SECRET,
+        sign: {
+            algorithm: JWT_ALGORITHM,
+        },
+        verify: {
+            algorithms: [JWT_ALGORITHM],
+        },
+    });
+
+    await app.register(fastifyCookie);
+
+    await app.register(prismaPlugin);
+    await app.register(userRoutes, { prefix: "/users" });
 
     return app;
 }
