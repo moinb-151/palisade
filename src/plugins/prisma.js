56bc345842386a4b3ff1612d70c74e603041ecad
Moin Bagban <moinbagban81@gmail.com>
2026-04-26 08:58:24 +0530

    minor changes

--- src/plugins/prisma.js
+++ src/plugins/prisma.js
@@ -1,5 +1,5 @@
 import fp from "fastify-plugin";
-import { PrismaClient } from "../../generated/prisma/client.ts";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
     log: ["error", "warn"],
